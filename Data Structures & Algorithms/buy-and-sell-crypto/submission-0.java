106ba72af99d388e2b1c7795644c56894b046c90
Praveena Sakthivelan <12373410+praveena-sakthi@users.noreply.github.com>
2026-07-12 18:59:35 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int idx = 1;
+        int maxProfit = 0;
+        int minVal  = prices[0];
+        int len = prices.length;
+        int min[] = new int[len];
+        for(int i=0;i<len;i++) {
+            minVal = Math.min(minVal,prices[i]);
+            min[i]=minVal;
+        }
+        int maxSoFar = 0;
+        for(int i=len-1;i>=0;i--) {
+            maxSoFar = Math.max(maxSoFar,prices[i]);
+            maxProfit = Math.max(maxSoFar-min[i], maxProfit);
+        }
+        return maxProfit < 0 ? 0 : maxProfit;
+    }
+}
