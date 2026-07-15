0f94ed484c7b78851a870e6740858367efb322cf
Praveena Sakthivelan <12373410+praveena-sakthi@users.noreply.github.com>
2026-07-15 21:57:24 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int left = 0, right = 0;
+        int freq [] = new int[26];
+        if(s1.length()>s2.length())
+        return false;
+        for(char c : s1.toCharArray()) {
+            freq[c-'a']++;
+        }
+        while(right < left+s1.length()) {
+            freq[s2.charAt(right)-'a']--;
+            right++;
+        }
+        while(left < s2.length()-s1.length()) {
+            System.out.println(left+" : "+Arrays.toString(freq));
+            if(isAllCharFound(freq)) {
+                return true;
+            }else {
+                freq[s2.charAt(left)-'a']++;
+                left++;
+                
+                freq[s2.charAt(right)-'a']--;
+                right++;
+            }
+        }
+        return isAllCharFound(freq);
+    }
+
+    private boolean isAllCharFound(int [] freq) {
+        for(int i=0;i<26;i++) {
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
